f7cbf34bf488b9231929f8ad60760fc82d9b7323
Clair <clair_griffiths@hotmail.com>
2018-01-07 16:05:22 +0000

    More Rubocop setup

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,22 +1,13 @@
-class Calendar < ActiveRecord::Base
+# frozen_string_literal: true
+
+class Calendar < ApplicationRecord
   has_many :calendar_days
   has_many :days, through: :calendar_days
-  
+
   serialize :days_of_weeks, Array
-  
-  
+
   def create_calendar_days
-   # result = (self.start_date..self.end_date).to_a.select {|k| @days_of_week.include?(k.wday)}
-    # iterate through and create day!
+
   end
-  
-  
-  
+
 end
-
-# start date
-# logic (is tue)
-# if yes create calendar_day
-# + 1.day
-
-# 
